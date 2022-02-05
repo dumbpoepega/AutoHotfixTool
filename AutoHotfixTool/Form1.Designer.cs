52521a766fd2e838cbdcb1aee22cc9ca23835cff
dumpoepega <tilter1337@gmail.com>
2022-02-05 17:45:34 +0100

    Initial commit

---
+++ AutoHotfixTool/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace AutoHotfixTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Apply = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hotfixLbl = new System.Windows.Forms.Label();
+            this.leagueLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Apply
+            // 
+            this.Apply.ForeColor = System.Drawing.Color.Black;
+            this.Apply.Location = new System.Drawing.Point(34, 71);
+            this.Apply.Name = "Apply";
+            this.Apply.Size = new System.Drawing.Size(161, 23);
+            this.Apply.TabIndex = 0;
+            this.Apply.Text = "Apply";
+            this.Apply.UseVisualStyleBackColor = true;
+            this.Apply.Click += new System.EventHandler(this.Apply_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click_1);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "League of Legends.exe Folder Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(474, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "League Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Cyan;
+            this.label3.Location = new System.Drawing.Point(317, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hotfix Applied";
+            // 
+            // hotfixLbl
+            // 
+            this.hotfixLbl.AutoSize = true;
+            this.hotfixLbl.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hotfixLbl.ForeColor = System.Drawing.Color.Red;
+            this.hotfixLbl.Location = new System.Drawing.Point(338, 42);
+            this.hotfixLbl.Name = "hotfixLbl";
+            this.hotfixLbl.Size = new System.Drawing.Size(36, 20);
+            this.hotfixLbl.TabIndex = 5;
+            this.hotfixLbl.Text = "false";
+            // 
+            // leagueLbl
+            // 
+            this.leagueLbl.AutoSize = true;
+            this.leagueLbl.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leagueLbl.ForeColor = System.Drawing.Color.Red;
+            this.leagueLbl.Location = new System.Drawing.Point(500, 42);
+            this.leagueLbl.Name = "leagueLbl";
+            this.leagueLbl.Size = new System.Drawing.Size(36, 20);
+            this.leagueLbl.TabIndex = 6;
+            this.leagueLbl.Text = "false";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(124, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(446, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Note: When selecting League game folder path, the location is usually like this:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(167, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(173, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(354, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "C:\\League of Legends\\Riot Games\\League of Legends\\Game\\";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(158, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(204, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(282, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Note that it depends where you installed League. ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(173, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(338, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "So just find the \'Game\' folder in LOL directory and select it.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(162, 259);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 20);
+            this.label10.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(478, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 20);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Unload Hotfix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(57, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 20);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Close League";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Bakbak One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label11.Location = new System.Drawing.Point(241, 123);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(224, 20);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Made by dumbpoepega#7459/Bogdan";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(656, 300);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.leagueLbl);
+            this.Controls.Add(this.hotfixLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Apply);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Apply;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hotfixLbl;
+        private System.Windows.Forms.Label leagueLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ AutoHotfixTool/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+using System.IO;
+
+namespace AutoHotfixTool
+{
+    public partial class Form1 : Form
+    {
+        private string hotfixPath;
+        private string leaguePath;
+        private string selfPath = AppDomain.CurrentDomain.BaseDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void WndProc(ref Message m) // Make form movable
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+    
+    private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Timers.Timer t1 = new System.Timers.Timer();
+            t1.Elapsed += T1_Elapsed;
+            t1.Enabled = true;
+            t1.Interval = 500;
+            t1.Start();
+            if (!File.Exists(selfPath + "config.txt"))
+            {
+                File.Create(selfPath + "config.txt");
+            }
+            else
+            {
+                textBox1.Text = File.ReadAllLines(selfPath + "config.txt")[0];
+            }
+            if (!Directory.Exists(selfPath + "Original Files"))
+            {
+                Directory.CreateDirectory(selfPath + "Original Files");
+            }
+        }
+
+        private void T1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (leagueOpened())
+            {
+                leagueLbl.ForeColor = Color.Lime;
+                leagueLbl.Invoke((MethodInvoker)(() => leagueLbl.Text = "opened"));
+            }
+            else
+            {
+                leagueLbl.ForeColor = Color.Red;
+                leagueLbl.Invoke((MethodInvoker)(() => leagueLbl.Text = "closed"));
+            }
+        }
+
+        private bool leagueOpened()
+        {
+            Process[] leagueList = Process.GetProcessesByName("LeagueClient");
+            if (leagueList.Length > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Apply_Click(object sender, EventArgs e)
+        {
+            if (leagueLbl.Text == "closed")
+            {
+                MessageBox.Show("League of Legends is closed. Please open it before applying hotfix.", "Error");
+                return;
+            }
+            if (textBox1.Text == "" || textBox1.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter your League of Legends.exe file path. Click on the textbox and select League Of Legends.com (not from Desktop but from C/D drive.)");
+                return;
+            }
+            try
+            {
+                
+                
+                File.Copy(textBox1.Text + @"\League of Legends.exe", selfPath + @"\Original Files\League of Legends.exe");
+                File.Copy(textBox1.Text + @"\stub.dll", selfPath + @"\Original Files\stub.dll");
+            }
+            catch
+            {
+
+            }
+
+            string[] hotfixFiles = Directory.GetFiles(selfPath + @"\Hotfix\"); 
+            string[] fileNames = { "League of Legends.exe", "stub.dll" };
+            File.Delete(textBox1.Text + @"\League of Legends.exe");
+            File.Delete(textBox1.Text + @"\stub.dll");
+            for (int i = 0; i < hotfixFiles.Length; i++)
+            {  
+                try
+                {
+                    File.Copy(hotfixFiles[i], textBox1.Text + @"\" + fileNames[i], true); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error has occured. " + ex.Message);
+                }
+            }
+            MessageBox.Show("Hotfix Applied.");
+            hotfixLbl.ForeColor = Color.Lime;
+            hotfixLbl.Text = "true";
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click_1(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog ofd = new FolderBrowserDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = ofd.SelectedPath;
+                    using (StreamWriter wr = new StreamWriter(selfPath + "config.txt"))
+                    {
+                        wr.WriteLine(textBox1.Text);
+                        
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Copy(selfPath + @"\Original Files\League of Legends.exe", textBox1.Text + @"\League of Legends.exe", true);
+                File.Copy(selfPath + @"\Original Files\stub.dll", textBox1.Text + @"\stub.dll", true);
+                MessageBox.Show("Successfully unloaded the hotfix.");
+                hotfixLbl.Text = "false";
+                hotfixLbl.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured. " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!leagueOpened())
+            {
+                MessageBox.Show("League of Legends is already closed.");
+                return;
+            }
+            Process[] pList = Process.GetProcessesByName("LeagueClient");
+            foreach (Process p in pList) { p.Kill(); }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have found an easter egg :D add me on discord for free eune acc to script on and send me a screenshot of this message window :D");
+        }
+    }
+}
